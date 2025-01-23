0e4d2591403db0cc7983bd88341b5d4c418c660d
Alan Rios <riosalan264@gmail.com>
2025-01-23 17:16:49 -0300

    feat: add architecture front

---
+++ coop-front/src/test/app/HomePage.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import HomePage from "@/app/page";
+import '@testing-library/jest-dom';
+
+describe("Homepage", () => {
+  it("Tiene que decir como deployar", () => {
+    render(<HomePage />);
+    expect(screen.getByText("Deploy now")).toBeInTheDocument();
+  });
+
+  it("Tiene que darnos la documentacion", () => {
+    render(<HomePage />);
+    expect(screen.getByText("Read our docs")).toBeInTheDocument();
+  });
+});
