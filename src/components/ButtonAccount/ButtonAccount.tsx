279a9ac869e2e17d1d450004be0ccee0f4a66a31
DelfinaSarkiss <delfysark17@gmail.com>
2025-04-02 19:42:35 -0300

    feat: button-account COMPLETE

--- src/components/ButtonAccount/ButtonAccount.tsx
+++ src/components/ButtonAccount/ButtonAccount.tsx
@@ -1,10 +1,10 @@
-import { PiUserCircleFill } from "react-icons/pi";
+import { FaRegUserCircle } from "react-icons/fa";
 
 export default function ButtonAccount() {
   return (
-    <button className="flex items-center gap-2 px-4 py-2 bg-white text-gray-900 rounded-lg shadow-md mt-10 mb-10 border border-gray-300 hover:bg-gray-100 transition">
-      <PiUserCircleFill size={24} />
-      <span>Mi cuenta</span>
+    <button className="flex items-center p-0 px-[17px] w-[160px] h-[48px] justify-center gap-2 py-2 bg-white text-[#0F172A] shadow-md mt-10 mb-10 border border-gray-300 rounded-[64px] hover:bg-gray-100 transition">
+      <FaRegUserCircle size={24} className="mr-1" />
+      <span className="mr-1">Mi cuenta</span>
     </button>
   );
 }
