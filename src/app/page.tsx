5d9a3225e57578ca09fd4db1b14ec0c9c7f55cd1
Facundo <facurael@gmail.com>
2025-08-19 16:13:03 -0300

    fix:Loader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,53 +9,42 @@ import {
   PartnerCarousel,
   AboutUs,
   Sucursal,
-  Loader
 } from "@/components";
-import {SucursalImage} from "@/public"; 
-
-import { useState } from "react";
+import { SucursalImage } from "@/public";
 
 export default function Home() {
-  const [loading, setLoading] = useState(true);
-
   return (
     <>
-        <Banner />
-      {loading ? (
-        <Loader onFinish={() => setLoading(false)} />
-      ) : (
-        <>
-          <PlansSection />
-          <PartnerCarousel />
-          <Service />
-          <AboutUs />
-          <Faqs />
-          <ContactForm
-            titulo="¿Tenés alguna duda?"
-            descripcion="Completá el formulario y contanos en qué podemos ayudarte. Ya sea una consulta, una sugerencia o un reclamo, tu mensaje es importante para nosotros."
-            horario="Respondemos de lunes a viernes, entre las 8 y las 16 h. ¡Te vamos a estar leyendo!"
-            mostrarLogo={true}
-          />
-          <Sucursal
-            titulo="Nuestras sucursales"
-              subtitulo="Más información"
-              sucursales={[
-                {
-                  nombre: "Sede central",
-                  direccion: "Av. El Callao 1328, Grand Bourg, Buenos Aires.",
-                  telefono: "Atención comercial 02320 - 483000",
-                  imagen: SucursalImage,
-                },
-                {
-                  nombre: "Sucursal Pablo Nogués",
-                  direccion: "Ejército de los Andes 2622 - Pablo Nogués.",
-                  telefono: "Atención comercial 02320 - 486000",
-                  imagen: SucursalImage,
-                },
-              ]}
-          />
-        </>
-      )}
+      <Banner />
+      <PlansSection />
+      <PartnerCarousel />
+      <Service />
+      <AboutUs />
+      <Faqs />
+      <ContactForm
+        titulo="¿Tenés alguna duda?"
+        descripcion="Completá el formulario y contanos en qué podemos ayudarte. Ya sea una consulta, una sugerencia o un reclamo, tu mensaje es importante para nosotros."
+        horario="Respondemos de lunes a viernes, entre las 8 y las 16 h. ¡Te vamos a estar leyendo!"
+        mostrarLogo={true}
+      />
+      <Sucursal
+        titulo="Nuestras sucursales"
+        subtitulo="Más información"
+        sucursales={[
+          {
+            nombre: "Sede central",
+            direccion: "Av. El Callao 1328, Grand Bourg, Buenos Aires.",
+            telefono: "Atención comercial 02320 - 483000",
+            imagen: SucursalImage,
+          },
+          {
+            nombre: "Sucursal Pablo Nogués",
+            direccion: "Ejército de los Andes 2622 - Pablo Nogués.",
+            telefono: "Atención comercial 02320 - 486000",
+            imagen: SucursalImage,
+          },
+        ]}
+      />
     </>
   );
 }
