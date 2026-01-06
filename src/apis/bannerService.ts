6f69164dcab2863c78af17d15f5259976b592b2f
Mauro Laime <maurol.dev@gmail.com>
2026-01-06 19:29:10 -0300

    fix: remove unnecessary console log from fetchGetBanners function

--- src/apis/bannerService.ts
+++ src/apis/bannerService.ts
@@ -3,13 +3,12 @@ import { BannerItem } from "../types/bannerItem";
 
 export const fetchGetBanners = async (): Promise<BannerItem[]> => {
   const res =   await fetch(`${process.env.NEXT_PUBLIC_API_URL}/banner`, {
-    cache: "no-store", // evita cacheo en dev
+    cache: "no-store", 
   });
 
   if (!res.ok) {
     throw new Error("Error al obtener los banners");
   }
 
-  console.log("Fetched banners:", await res.clone().json());
   return res.json();
 };
